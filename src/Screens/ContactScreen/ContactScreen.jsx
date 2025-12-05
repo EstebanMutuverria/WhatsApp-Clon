c045bac908532a973a7d613ae671e8eac57bb49f
estban03 <estebanmutuverria003@gmail.com>
2025-12-04 23:45:33 -0300

    changes in css styles

--- src/Screens/ContactScreen/ContactScreen.jsx
+++ src/Screens/ContactScreen/ContactScreen.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ContactSidebar from '../../Components/ContactSidebar/ContactSidebar'
 import LeftSidebar from '../../Components/LeftSidebar/LeftSidebar'
+import './ContactScreen.css'
 
 export default function ContactScreen() {
   return (
-    <div>
+    <div className='contact-screen-container'>
         <LeftSidebar />
         <ContactSidebar />
     </div>
