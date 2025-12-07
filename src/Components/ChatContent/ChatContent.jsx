efd237a5c84001f52b5d41151419895d1ff45649
estban03 <estebanmutuverria003@gmail.com>
2025-12-07 19:00:47 -0300

    add Component ChatContent.jsx and css

---
+++ src/Components/ChatContent/ChatContent.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './ChatContent.css'
+
+export default function ChatContent({chat}) {
+  return (
+    <div className='chat-content-container'>
+        <header className='chat-content-header'>
+            <img src="" alt="" />
+            <a href="">{}</a>
+            <button></button>
+            <button></button>
+            <button></button>
+        </header>
+    </div>
+  )
+}
