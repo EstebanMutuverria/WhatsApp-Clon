03f3d6d4a6c610e18096873aa12f277a7a6baa7b
estban03 <estebanmutuverria003@gmail.com>
2025-12-10 09:15:53 -0300

    changes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,20 @@ import { Routes, Route } from "react-router"
 import ChatScreen from "./Screens/ChatScreen/ChatScreen"
 import MessagesScreen from "./Screens/MessagesScreen/MessagesScreen"
 import './Global.css'
+import ContactListContextProvider from "./Contexts/ContactListContext"
+import ContactDetailContextProvider from "./Contexts/ContactDetailContext"
 
 function App() {
 
   return (
     <div>
       <Routes>
-        <Route path="/" element={<ChatScreen/>} />
-        <Route path="/chat/:contact_id" element={<MessagesScreen/>} />
+        <Route element = {<ContactListContextProvider/>}>
+          <Route path="/" element={<ChatScreen/>} />
+          <Route path="/chat/:contact_id" element={<ContactDetailContextProvider/>}>
+            <Route path="/chat/:contact_id/messages" element={<MessagesScreen/>} />
+          </Route>
+        </Route>
       </Routes>
     </div>
   )
