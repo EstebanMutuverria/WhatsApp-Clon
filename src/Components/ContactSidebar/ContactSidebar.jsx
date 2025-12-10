e66fec311e9a6a8ab62043afcf58663f1380f100
estban03 <estebanmutuverria003@gmail.com>
2025-12-10 09:17:52 -0300

    changes in ContactSidebar.jsx

--- src/Components/ContactSidebar/ContactSidebar.jsx
+++ src/Components/ContactSidebar/ContactSidebar.jsx
@@ -1,47 +1,13 @@
-import React, {useEffect } from 'react'
-import { useState } from 'react'
 import ContactSearchForm from '../ContactSearchForm/ContactSearchForm'
 import ContactList from '../ContactList/ContactList'
-import { getContactList } from '../../Services/ContactService'
 import './ContactSidebar.css'
 import ContactFilters from '../ContactFilters/ContactFilter'
+import { ContactListContext } from '../../Contexts/ContactListContext'
 /* 
 snippet para crear un componente de react
 RFC = React fuctional component 
 */
 export default function ContactSidebar() {
-    const [contactState, setContactState] = useState([])
-    const [loadingContactState, setLoadingContactState] = useState(true)
-    
-    function loadContactList(){
-        setLoadingContactState(true);
-        /* setTimeout nos permite atrasar la ejecucion de otra funcion y recibe 2 parametros:
-        1- la accion a retrasar
-        2- los milisegundos que quiero que se retrase la ejecucion
-        */
-        setTimeout(
-            function(){
-                console.log("Cargando Lista de contactos")
-                //Cargo la respuesta del servidor
-                const contact_list = getContactList();
-                //Guardo la respuesta
-                setContactState(contact_list);
-                setLoadingContactState(false)
-            },2000
-        )
-    }
-
-    /* La funcion useEffect nos permite controlar cuantas veces una funcion sera invocada 
-    useEffect espera recibir dos parametros:
-    1 = La funcion que queremos controlar.
-    2 = Las dependencias. Es una lista de estados. Si la lista esta vacia "[]" solo se ejecutara 
-    una sola vez
-
-    */
-    useEffect(loadContactList,[])
-
-    console.log(loadingContactState, contactState)
-
     return (
         <aside className='contact-side-bar-container'>
             <header className='header'>
@@ -60,7 +26,7 @@ export default function ContactSidebar() {
                 <ContactFilters />
             </div>
             <div className='contact-list-scroll'>
-                <ContactList contactState = {contactState} loadingContactState = {loadingContactState}/>
+                <ContactList />
             </div>
         </aside>
     )
