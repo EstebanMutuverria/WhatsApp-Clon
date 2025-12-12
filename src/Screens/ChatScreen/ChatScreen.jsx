b9797b3d93e129157b3878a3222a322a1840432a
estban03 <estebanmutuverria003@gmail.com>
2025-12-12 03:07:43 -0300

    changes in ChatScreen.jsx and css

--- src/Screens/ChatScreen/ChatScreen.jsx
+++ src/Screens/ChatScreen/ChatScreen.jsx
@@ -1,10 +1,28 @@
-import React, { useContext } from 'react'
+import React, { useEffect, useState } from 'react'
 import ContactSidebar from '../../Components/ContactSidebar/ContactSidebar'
 import LeftSidebar from '../../Components/LeftSidebar/LeftSidebar'
 import './ChatScreen.css'
 import { ContactListContext } from '../../Contexts/ContactListContext'
 
 export default function ContactScreen() {
+    const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    // Simula llamada al servidor
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 2000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="loader-overlay">
+        <img src="./cargando/grey-9026.gif" alt="Cargando..." />
+      </div>
+    )
+  }
   return (
     <div className='chat-screen-container'>
         <LeftSidebar />
