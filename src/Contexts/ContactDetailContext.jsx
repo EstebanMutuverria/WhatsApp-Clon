a7c25c5d3184133d09e93a03cb825fbcd63eda19
estban03 <estebanmutuverria003@gmail.com>
2025-12-17 03:14:41 -0300

    changes in ContactDetailContext and CntactListContext

--- src/Contexts/ContactDetailContext.jsx
+++ src/Contexts/ContactDetailContext.jsx
@@ -1,37 +1,34 @@
-import { createContext, useEffect, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 import { Outlet, useParams } from "react-router";
-import { getContactById } from "../Services/ContactService";
+import { ContactListContext } from "./ContactListContext";
 
 export const ContactDetailContext = createContext();
 
-const ContactDetailContextProvider = () =>{
-        const parametros_url = useParams();
-        const contact_id = parametros_url.contact_id;
-        const [contactSelected, setContactSelected] = useState(null);
-        const [loadingContact, setLoadingContact] = useState(true);
-    
-        function loadContactById(){
-            setLoadingContact(true);
-            //Simulamos la carga con un setTimeout
-            setTimeout(
-                function(){
-                    const contact = getContactById(contact_id)
-                    setContactSelected(contact);
-                    setLoadingContact(false);
-                },
-                2000
-            )
-        }
-        useEffect(loadContactById, [parametros_url.contact_id])
-    
+const ContactDetailContextProvider = () => {
+    const parametros_url = useParams();
+    const contact_id = parametros_url.contact_id;
+    const { contactState, getContactById, updateContactById } = useContext(ContactListContext);
+    const contactSelected = getContactById(contact_id);
+    const [loadingContact, setLoadingContact] = useState(true);
+
+    function loadContactById() {
+        setLoadingContact(true);
+        //Simulamos la carga con un setTimeout
+        setTimeout(
+            function () {
+                setLoadingContact(false);
+            },
+            2000
+        )
+    }
+    useEffect(loadContactById, [parametros_url.contact_id])
+
     const providerValues = {
         contactSelected,
-        loadingContact,
-        loadContactById,
         addNewMessage
     }
 
-    function addNewMessage(content){
+    function addNewMessage(content) {
         const newMessage = {
             message_id: contactSelected.messages.length + 1,
             message_content: content,
@@ -40,7 +37,7 @@ const ContactDetailContextProvider = () =>{
             send_by_me: true
         }
 
-        setContactSelected({
+        updateContactById(contact_id, {
             ...contactSelected,
             messages: [...contactSelected.messages, newMessage]
         });
@@ -48,7 +45,7 @@ const ContactDetailContextProvider = () =>{
 
     return (
         <ContactDetailContext.Provider value={providerValues}>
-            <Outlet/>
+            <Outlet />
         </ContactDetailContext.Provider>
     )
 }
