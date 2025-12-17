f0c0eb4093204c522b66fed4570fc6c37ce130d5
estban03 <estebanmutuverria003@gmail.com>
2025-12-17 03:15:01 -0300

    changes in MessageScreen.jsx

--- src/Screens/MessagesScreen/MessagesScreen.jsx
+++ src/Screens/MessagesScreen/MessagesScreen.jsx
@@ -1,28 +1,31 @@
-import React, { useContext} from 'react'
+import React, { useContext } from 'react'
 import ContactSidebar from '../../Components/ContactSidebar/ContactSidebar'
 import LeftSidebar from '../../Components/LeftSidebar/LeftSidebar'
 import './MessagesScreen.css'
-import {ContactDetailContext} from '../../Contexts/ContactDetailContext'
+import { ContactDetailContext } from '../../Contexts/ContactDetailContext'
 import ChatContent from '../../Components/ChatContent/ChatContent'
 import AddNewMessage from '../../Components/AddNewMesagge/AddNewMessage'
+import { ContactListContext } from '../../Contexts/ContactListContext'
 
 export default function MessagesScreen() {
-    const {loadingContact, contactSelected} = useContext(ContactDetailContext);
-    console.log(loadingContact, contactSelected);
+    const { contactSelected } = useContext(ContactDetailContext);
+    console.log(contactSelected);
+
+    const { loadingContactState } = useContext(ContactListContext);
     return (
         <div className='messages-screen-container'>
             <LeftSidebar />
             <ContactSidebar />
             {
-                loadingContact ? 
-                <div className='loader-chats'>
-                    <img src='https://cdn.pixabay.com/animation/2023/11/09/03/05/03-05-45-320_512.gif' alt='Cargando contacto...'></img>
-                </div>
-                : 
-                <div className='messages-screen-chat-content'>
-                    <ChatContent />
-                    <AddNewMessage />
-                </div>
+                loadingContactState ?
+                    <div className='loader-chats'>
+                        <img src='https://cdn.pixabay.com/animation/2023/11/09/03/05/03-05-45-320_512.gif' alt='Cargando contacto...'></img>
+                    </div>
+                    :
+                    <div className='messages-screen-chat-content'>
+                        <ChatContent />
+                        <AddNewMessage />
+                    </div>
             }
         </div>
     )
